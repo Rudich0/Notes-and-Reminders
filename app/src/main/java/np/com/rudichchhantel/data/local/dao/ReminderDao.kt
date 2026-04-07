0a33be930102539ecb29fbfcf4fc02aac228fdd7
Rudich0 <r7dich@gmail.com>
2026-04-07 18:37:49 +0545

    created a proper database for testing

--- app/src/main/java/np/com/rudichchhantel/data/local/dao/ReminderDao.kt
+++ app/src/main/java/np/com/rudichchhantel/data/local/dao/ReminderDao.kt
@@ -1,2 +1,20 @@
 package np.com.rudichchhantel.data.local.dao
 
+import androidx.room.*
+import np.com.rudichchhantel.data.local.entity.Reminder
+
+@Dao
+interface ReminderDao {
+
+    @Query("SELECT * FROM reminders")
+    fun getAllReminders(): List<Reminder>
+
+    @Insert
+    fun insert(reminder: Reminder)
+
+    @Update
+    fun update(reminder: Reminder)
+
+    @Delete
+    fun delete(reminder: Reminder)
+}
--- app/src/main/java/np/com/rudichchhantel/data/local/entity/Reminder.kt
+++ app/src/main/java/np/com/rudichchhantel/data/local/entity/Reminder.kt
@@ -1,2 +1,12 @@
 package np.com.rudichchhantel.data.local.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "reminders")
+data class Reminder(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val time: String
+)
